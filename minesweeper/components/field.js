4a978c864201faafcbe77a8c582a3dbcd6786e8c
Olitera <lu_gu@mail.ru>
2023-05-18 14:00:20 +0200

    refactor: write functions in class field

--- minesweeper/components/field.js
+++ minesweeper/components/field.js
@@ -1,30 +1,40 @@
+import Cell from "./cell.js";
+
+
 class Field {
   body = document.querySelector('body');
   game;
   fieldSize = 10;
+  cellMatrix;
   
 
   createField() {
     this.game = document.createElement('div');
     this.game.className = 'game';
     this.body.append(this.game);
+    this.updateField();
   }
 
   updateField(number) {
     this.game.innerHtml = '';
-    this.createSquareArray(number)
-    this.createLine
-
+    this.updateFieldSize(number);
+    // this.createSquareArray();
+    // this.createLine();
+    // console.log(this.createMinesweeper(this.createSquareArray()));
+    this.cellMatrix = this.createMatrix(this.createSquareArray());
   }
 
 
 
-  checkFieldSize(number) {
-    this.fieldSize = number;
+  updateFieldSize(number) {
+    if(number) {
+      this.fieldSize = number;  
+    }
   }
 
   createSquareArray() {
-    return Array.from(Array(this.fieldSize), () => Array.from({ length: 9 }, (_, index) => false).concat(Array.from({ length: 1 }, (_, index) => true)).sort(() => Math.random() - 0.5));
+    this.matrix = Array.from(Array(this.fieldSize), () => Array.from({ length: 9 }, (_, index) => false).concat(Array.from({ length: 1 }, (_, index) => true)).sort(() => Math.random() - 0.5));
+    return this.matrix;
   }
 
   createLine() {
@@ -34,78 +44,67 @@ class Field {
     return square;
   }
 
-  createMinesweeper(matrix) {
-    function checkEl(a, b) {
-      let startI = a > 0 ? a - 1 : 0;
-      let startJ = b > 0 ? b - 1 : 0;
-      let finishI = a < matrix.length - 1 ? a + 1 : a;
-      let finishJ = b < matrix[0].length - 1 ? b + 1 : b;
-      let sum = 0;
-      for (let i = startI; i <= finishI; i++) {
-        for (let j = startJ; j <= finishJ; j++) {
-          if (matrix[i][j] === true && (i !== a || j !== b)) {
-            sum++;
-          }
-        }
-      }
-      return sum
-    }
-    let arr = [];
-    for (let i = 0; i < matrix.length; i++) {
-      arr.push(new Array());
-      for (let j = 0; j < matrix[i].length; j++) {
-        arr[i].push(checkEl(i, j));
-      }
-    }
-    // console.log(arr);
-    return arr
+  createMatrix(array) {
+    return array.map((line, i) => {
+      // this.game.innerHtml = '';
+      console.log(array, line)
+      const lineElement = this.createLine();
+      return line.map((el, j) => { 
+        console.log(i,j, array, this.checkEl(i,j));
+        const cell = new Cell(lineElement, this.checkEl(i,j), el);
+        console.log(cell)
+        cell.createCell();
+        return cell;
+      });
+    })
   }
-}
 
-
-const squareArray = Array.from(Array(10), () => Array.from({ length: 9 }, (_, index) => false).concat(Array.from({ length: 1 }, (_, index) => true)).sort(() => Math.random() - 0.5));
-
-function createLine() {
-  const square = document.createElement('div');
-  square.className = 'line';
-  game.append(square);
-  return square;
-}
-// console.log(squareArray);
-
-function minesweeper(matrix) {
-  function checkEl(a, b) {
+  checkEl(a, b) {
     let startI = a > 0 ? a - 1 : 0;
     let startJ = b > 0 ? b - 1 : 0;
-    let finishI = a < matrix.length - 1 ? a + 1 : a;
-    let finishJ = b < matrix[0].length - 1 ? b + 1 : b;
+    let finishI = a < this.matrix.length - 1 ? a + 1 : a;
+    let finishJ = b < this.matrix[0].length - 1 ? b + 1 : b;
     let sum = 0;
     for (let i = startI; i <= finishI; i++) {
       for (let j = startJ; j <= finishJ; j++) {
-        if (matrix[i][j] === true && (i !== a || j !== b)) {
+        if (this.matrix[i][j] === true && (i !== a || j !== b)) {
           sum++;
         }
       }
     }
     return sum
   }
-  let arr = [];
-  for (let i = 0; i < matrix.length; i++) {
-    arr.push(new Array());
-    for (let j = 0; j < matrix[i].length; j++) {
-      arr[i].push(checkEl(i, j));
-    }
-  }
-  // console.log(arr);
-  return arr
+
+  // createMinesweeper(matrix) {
+  //   function checkEl(a, b) {
+  //     let startI = a > 0 ? a - 1 : 0;
+  //     let startJ = b > 0 ? b - 1 : 0;
+  //     let finishI = a < matrix.length - 1 ? a + 1 : a;
+  //     let finishJ = b < matrix[0].length - 1 ? b + 1 : b;
+  //     let sum = 0;
+  //     for (let i = startI; i <= finishI; i++) {
+  //       for (let j = startJ; j <= finishJ; j++) {
+  //         if (matrix[i][j] === true && (i !== a || j !== b)) {
+  //           sum++;
+  //         }
+  //       }
+  //     }
+  //     return sum
+  //   }
+    
+  //   return this.createMatrix(matrix); 
+  //   // let arr = [];
+  //   // for (let i = 0; i < matrix.length; i++) {
+  //   //   arr.push(new Array());
+  //   //   for (let j = 0; j < matrix[i].length; j++) {
+  //   //     arr[i].push(checkEl(i, j));
+  //   //   }
+  //   // }
+  //   // console.log(arr);
+  //   // return arr
+  // }
 }
 
-const cellMatrix = minesweeper(squareArray).map((line, i) => {
-  game.innerHtml = '';
-  const lineElement = createLine();
-  return line.map((el, j) => { 
-    const cell = new Cell(lineElement, el, squareArray[i][j]);
-    cell.createCell();
-    return cell;
-  });
-})
+export default Field;
+
+
--- minesweeper/index.js
+++ minesweeper/index.js
@@ -1,4 +1,5 @@
-import Cell from "./components/cell.js";
+// import Cell from "./components/cell.js";
+import Field from "./components/field.js";
 
 const body = document.querySelector('body');
 
@@ -20,62 +21,14 @@ const move = document.createElement('div');
 move.className = "move";
 character.append(move);
 
-// const game = document.createElement('div');
-// game.className = 'game';
-// body.append(game);
+
+const field = new Field;
+field.createField();
 
 const restart = document.createElement('button');
 restart.className = 'restart';
 restart.innerText = 'New game';
 body.append(restart);
-restart.addEventListener('click', );
+// restart.addEventListener('click', this.updateField());
 
-const squareArray = Array.from(Array(10), () => Array.from({ length: 9 }, (_, index) => false).concat(Array.from({ length: 1 }, (_, index) => true)).sort(() => Math.random() - 0.5));
 
-function createLine() {
-  const square = document.createElement('div');
-  square.className = 'line';
-  game.append(square);
-  return square;
-}
-// console.log(squareArray);
-
-function minesweeper(matrix) {
-  function checkEl(a, b) {
-    let startI = a > 0 ? a - 1 : 0;
-    let startJ = b > 0 ? b - 1 : 0;
-    let finishI = a < matrix.length - 1 ? a + 1 : a;
-    let finishJ = b < matrix[0].length - 1 ? b + 1 : b;
-    let sum = 0;
-    for (let i = startI; i <= finishI; i++) {
-      for (let j = startJ; j <= finishJ; j++) {
-        if (matrix[i][j] === true && (i !== a || j !== b)) {
-          sum++;
-        }
-      }
-    }
-    return sum
-  }
-  let arr = [];
-  for (let i = 0; i < matrix.length; i++) {
-    arr.push(new Array());
-    for (let j = 0; j < matrix[i].length; j++) {
-      arr[i].push(checkEl(i, j));
-    }
-  }
-  // console.log(arr);
-  return arr
-}
-
-const cellMatrix = minesweeper(squareArray).map((line, i) => {
-  game.innerHtml = '';
-  const lineElement = createLine();
-  return line.map((el, j) => { 
-    const cell = new Cell(lineElement, el, squareArray[i][j]);
-    cell.createCell();
-    return cell;
-  });
-})
-
-// cellMatrix[5][5].cellElement.innerText = cellMatrix[5][5].value;
-console.log(cellMatrix);

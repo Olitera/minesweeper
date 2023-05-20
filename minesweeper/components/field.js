e8477ce8696671b41d4ed8e521e7a7de7d1713fa
Olitera <lu_gu@mail.ru>
2023-05-20 12:02:49 +0200

    feat: add show to duration game

--- minesweeper/components/field.js
+++ minesweeper/components/field.js
@@ -25,148 +25,166 @@ function onClick(cell) {
   }
 }
 class Field {
-  clickCount = 0;
   body = document.querySelector('body');
   game;
   fieldSize = 10;
   cellMatrix;
+  clickCount = 0;
+  gameTime = 0;
+  timeout;
+  audioClick = new Audio("./assets/click.mp3");
+  
 
-  createField() {
-    this.game = document.createElement('div');
-    this.game.className = 'game';
-    this.body.append(this.game);
-    this.updateField();
+
+
+createField() {
+  this.game = document.createElement('div');
+  this.game.className = 'game';
+  this.body.append(this.game);
+  this.updateField();
+}
+
+updateField(number) {
+  this.clickCount = 0;
+  document.querySelector('.move').innerText = this.clickCount;
+  this.gameTime = 0;
+  document.querySelector('.time').innerText = this.gameTime;
+  this.game.innerText = '';
+  this.game.removeEventListener('click', this.onFieldClick);
+  this.updateFieldSize(number);
+  this.cellMatrix = this.createMatrix(this.matrix);
+  this.game.addEventListener('click', this.onFieldClick);
+//   this.audio.innerHTML = "<source src=\"assets/click.mp3\" type=\"audio/mpeg\">";
+//   document.body.appendChild(audio);
+//   this.game.addEventListener('click', this.audio.play);
+}
+
+updateFieldSize(number) {
+  if (number) {
+    this.fieldSize = number;
   }
+  this.createSquareArray();
+}
 
-  updateField(number) {
-    this.clickCount = 0;
-    document.querySelector('.move').innerText = this.clickCount;
-    this.game.innerText = '';
-    this.game.removeEventListener('click', this.onFieldClick);
-    this.updateFieldSize(number);
-    this.cellMatrix = this.createMatrix(this.matrix);
-    this.game.addEventListener('click', this.onFieldClick);
-  }
-
-  updateFieldSize(number) {
-    if (number) {
-      this.fieldSize = number;
-    }
-    this.createSquareArray();
-  }
-
-  createSquareArray() {
-    this.matrix = Array.from(Array(this.fieldSize), () => Array.from({ length: 9 }, (_, index) => false).concat(Array.from({ length: 1 }, (_, index) => true)).sort(() => Math.random() - 0.5));
-    return this.matrix;
-  }
+createSquareArray() {
+  this.matrix = Array.from(Array(this.fieldSize), () => Array.from({ length: 9 }, (_, index) => false).concat(Array.from({ length: 1 }, (_, index) => true)).sort(() => Math.random() - 0.5));
+  return this.matrix;
+}
 
 
-  onFieldClick = (event) => {
-    this.cellMatrix.forEach((el, i) => {
-      el.forEach((e, j) => {
-        if (e.cellElement === event.target) {
-          this.clickFunction(i, j);
-          if(e.isBomb) {
-            this.showBombs();
-          }
-          for(let i = 0; i< 9000; i++) {
-            this.openFreeCells();
-          }
-        }
-      })
-    });
-  }
-
-  openFreeCell(e) {
-    if (!e.isOpen && !e.isFlag && !e.isBomb) {
-      onClick(e);
-      e.isOpen = true;
-    }
-  }
-
-  openFreeCells() {
-    this.cellMatrix.forEach((el, i) => {
-      el.forEach((e, j) => {
-        if (e.isOpen && e.value === 0 && !e.isBomb) {
-          this.checkFree(i, j);
-        }
-      })
-    });
-    
-  }
-
-  showBombs() {
-    this.cellMatrix.forEach((el, i) => {
-      el.forEach((e, j) => {
-
+onFieldClick = (event) => {
+  this.cellMatrix.forEach((el, i) => {
+    el.forEach((e, j) => {
+      if (e.cellElement === event.target) {
+        this.clickFunction(i, j);
         if (e.isBomb) {
-          e.cellElement.style.backgroundImage = `url('./assets/bomb.svg')`
+          this.showBombs();
         }
-      })
-    });
-  }
-
-  clickFunction(i, j) {
-    const e = this.cellMatrix[i][j];
-    if (e.isBomb && this.clickCount === 0) {
-      console.log('first BOMB!!!')
-      this.updateField();
-      this.clickFunction(i, j);
-    } else {
-      onClick(e);
-      if (!e.isOpen) {
-        this.clickCount = this.clickCount + 1;
+        for (let i = 0; i < 9000; i++) {
+          this.openFreeCells();
+        }
       }
-      e.isOpen = true;
-      document.querySelector('.move').innerText = this.clickCount;
-    }
-  }
-
-  createLine() {
-    const square = document.createElement('div');
-    square.className = 'line';
-    this.game.append(square);
-    return square;
-  }
-
-  createMatrix(array) {
-    return array.map((line, i) => {
-      const lineElement = this.createLine();
-      return line.map((el, j) => {
-        const cell = new Cell(lineElement, this.checkEl(i, j), el);
-        cell.createCell();
-        return cell;
-      });
     })
-  }
+  });
+}
 
-  checkEl(a, b) {
-    let startI = a > 0 ? a - 1 : 0;
-    let startJ = b > 0 ? b - 1 : 0;
-    let finishI = a < this.matrix.length - 1 ? a + 1 : a;
-    let finishJ = b < this.matrix[0].length - 1 ? b + 1 : b;
-    let sum = 0;
-    for (let i = startI; i <= finishI; i++) {
-      for (let j = startJ; j <= finishJ; j++) {
-        if (this.matrix[i][j] === true && (i !== a || j !== b)) {
-          sum++;
-        }
+openFreeCell(e) {
+  if (!e.isOpen && !e.isFlag && !e.isBomb) {
+    onClick(e);
+    e.isOpen = true;
+  }
+}
+
+openFreeCells() {
+  this.cellMatrix.forEach((el, i) => {
+    el.forEach((e, j) => {
+      if (e.isOpen && e.value === 0 && !e.isBomb) {
+        this.checkFree(i, j);
+      }
+    })
+  });
+
+}
+
+showBombs() {
+  if (this.clickCount > 1) {
+  this.cellMatrix.forEach((el, i) => {
+    el.forEach((e, j) => {
+
+      if (e.isBomb) {
+        e.cellElement.style.backgroundImage = `url('./assets/bomb.svg')`
+      }
+    })
+  })
+}
+}
+
+clickFunction(i, j) {
+  const e = this.cellMatrix[i][j];
+  if (e.isBomb && this.clickCount === 0) {
+    console.log('first BOMB!!!')
+    this.updateField();
+    this.clickFunction(i, j);
+  } else {
+    onClick(e);
+    if (!e.isOpen) {
+      this.clickCount = this.clickCount + 1;
+      if(!e.isBomb) {
+      this.audioClick.play();
+      } else {
+
       }
     }
-    return sum
+    e.isOpen = true;
+    document.querySelector('.move').innerText = this.clickCount;
   }
+}
 
-  checkFree(a, b) {
-    let startI = a > 0 ? a - 1 : 0;
-    let startJ = b > 0 ? b - 1 : 0;
-    let finishI = a < this.matrix.length - 1 ? a + 1 : a;
-    let finishJ = b < this.matrix[0].length - 1 ? b + 1 : b;
-    for (let i = startI; i <= finishI; i++) {
-      for (let j = startJ; j <= finishJ; j++) {
-        this.openFreeCell(this.cellMatrix[i][j]);
+createLine() {
+  const square = document.createElement('div');
+  square.className = 'line';
+  this.game.append(square);
+  return square;
+}
+
+createMatrix(array) {
+  return array.map((line, i) => {
+    const lineElement = this.createLine();
+    return line.map((el, j) => {
+      const cell = new Cell(lineElement, this.checkEl(i, j), el);
+      cell.createCell();
+      return cell;
+    });
+  })
+}
+
+checkEl(a, b) {
+  let startI = a > 0 ? a - 1 : 0;
+  let startJ = b > 0 ? b - 1 : 0;
+  let finishI = a < this.matrix.length - 1 ? a + 1 : a;
+  let finishJ = b < this.matrix[0].length - 1 ? b + 1 : b;
+  let sum = 0;
+  for (let i = startI; i <= finishI; i++) {
+    for (let j = startJ; j <= finishJ; j++) {
+      if (this.matrix[i][j] === true && (i !== a || j !== b)) {
+        sum++;
       }
     }
   }
+  return sum
+}
+
+checkFree(a, b) {
+  let startI = a > 0 ? a - 1 : 0;
+  let startJ = b > 0 ? b - 1 : 0;
+  let finishI = a < this.matrix.length - 1 ? a + 1 : a;
+  let finishJ = b < this.matrix[0].length - 1 ? b + 1 : b;
+  for (let i = startI; i <= finishI; i++) {
+    for (let j = startJ; j <= finishJ; j++) {
+      this.openFreeCell(this.cellMatrix[i][j]);
+    }
+  }
+}
 
 }
 
--- minesweeper/index.js
+++ minesweeper/index.js
@@ -20,15 +20,38 @@ character.append(moveText);
 const move = document.createElement('div');
 move.className = "move";
 character.append(move);
-
+const restart = document.createElement('button');
+restart.className = 'restart';
+restart.innerText = 'New game';
+character.append(restart);
+restart.addEventListener('click', () => {field.game.innerHtml="";field.updateField();});
 
 const field = new Field;
 field.createField();
 
-const restart = document.createElement('button');
-restart.className = 'restart';
-restart.innerText = 'New game';
-body.append(restart);
-restart.addEventListener('click', () => {field.game.innerHtml="";field.updateField();});
+const intervalID = setInterval(()=>{
+  const currentTime = 1 +  +time.innerText;
+  time.innerText = `${currentTime}`;
+}, 1000);
+
+window.addEventListener('click', ()=>clearInterval(intervalID));
+
+const sound = document.createElement('button');
+sound.className = 'sound';
+sound.innerText = 'Sound';
+body.append(sound);
+
+
+
+
+// console.log (`
+// -design, responsive UI
+// -the game should end when the player reveals all cells that do not contain mines (win) or clicks on mine (lose) and related message is displayed at the end of the game
+// -mines are placed after the first move, so that user cannot lose on the first move
+// -user can mark “mined” cells using flags so as not to accidentally open them displaying the number of mines remaining and displaying number of used flags
+// -game duration and number of clicks are displayed
+// -sound accompaniment (on/off) when clicking on cell and at the end of the game
+// -option to choose different themes for the game board (dark/light themes)
+// `)
 
 

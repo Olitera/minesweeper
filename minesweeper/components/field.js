9fcf747f37096ef37535eb1d1df4d6d5daa499bc
Olitera <lu_gu@mail.ru>
2023-05-22 14:40:07 +0200

    feat: add table of results

--- minesweeper/components/field.js
+++ minesweeper/components/field.js
@@ -18,15 +18,18 @@ function onClick(cell) {
     case 3:
       cell.cellElement.classList.add('red');
       break;
-
-  }
-  if (cell.isBomb) {
-    cell.cellElement.innerText = '';
-    cell.cellElement.style.backgroundImage = `url('./assets/mine.svg')`;
-    cell.cellElement.style.backgroundColor = 'red';
-    remarkText.innerText = 'Game over. Try again';
-    remarkText.style.color = 'red';
-    remark.style.display = 'flex';
+    case 4:
+      cell.cellElement.classList.add('purple');
+      break;
+    case 5:
+      cell.cellElement.classList.add('yellow');
+      break;
+    case 6:
+      cell.cellElement.classList.add('pink');
+      break;
+    case 7:
+      cell.cellElement.classList.add('orange');
+      break;
   }
 }
 class Field {
@@ -47,28 +50,31 @@ class Field {
   gameTime = 0;
   minesCount = 10;
   flagCount = 0;
-  timeout;
+  timer;
+  currentTime;
   audioClick = new Audio('./assets/click.mp3');
   audioEnd = new Audio('./assets/gameOver.mp3');
   audioWin = new Audio('./assets/win.mp3');
 
   createField() {
+    const option = document.querySelector('.option'); 
     this.game = document.createElement('div');
     this.game.className = 'game';
-    this.body.append(this.game);
+    option.before(this.game);
     this.updateField();
+    this.fillTable(this.getDataFromLS());
   }
 
   updateField(number) {
+    this.stopTimer();
+    this.remark.style.display = 'none';
     this.clickCount = 0;
     this.move.innerText = this.clickCount;
     this.gameTime = 0;
     this.time.innerText = this.gameTime;
-    // this.minesCount = 10;
     this.mine.innerText = this.minesCount;
     this.flagCount = 0;
     this.flagNumber.innerText = this.flagCount;
-
     this.game.innerText = '';
     this.game.removeEventListener('click', this.onFieldClick);
     this.game.removeEventListener('contextmenu', this.onFieldClick);
@@ -83,11 +89,15 @@ class Field {
     this.game.addEventListener('contextmenu', this.onFieldClick);
     this.size.addEventListener('change', (e) => this.updateField(+this.size.value));
     this.mineVaryation.addEventListener('change', () => {
-      this.minesCount = this.mineVaryation.value
+      if (this.mineVaryation.value < 10) {
+        this.mineVaryation.value = 10;
+      } else if (this.mineVaryation.value > 99) {
+        this.mineVaryation.value = 99;
+      }
+      this.minesCount = this.mineVaryation.value;
       this.mine.innerText = this.minesCount - this.flagCount;
+      this.updateField();
     });
-
-
 
     localStorage.setItem('matrix', JSON.stringify(this.cellMatrix));
   }
@@ -117,6 +127,10 @@ class Field {
 
 
   onFieldClick = (event) => {
+    console.log(this.clickCount);
+    if (this.clickCount === 0) {
+      this.startTimer();
+    }
     this.cellMatrix.forEach((el, i) => {
       el.forEach((e, j) => {
         if (e.cellElement === event.target) {
@@ -132,6 +146,7 @@ class Field {
             this.flagNumber.innerText = this.flagCount;
             this.mine.innerText = this.minesCount - this.flagCount > 0 ? this.minesCount - this.flagCount : 0;
           } else if (event.button == 0 && event.type === 'click' && !e.isFlag) {
+            this.gameTime = reset();
             this.clickFunction(i, j);
             if (e.isBomb) {
               this.showBombs();
@@ -171,13 +186,12 @@ class Field {
             e.cellElement.style.backgroundImage = `url('./assets/bomb.svg')`
           }
           if (e.isFlag && !e.isBomb) {
-            e.cellElement.style.backgroundColor = 'red';
+            e.cellElement.style.backgroundColor = '#e49191';
           }
         })
       })
     }
   }
-
 
   clickFunction(i, j) {
     const e = this.cellMatrix[i][j];
@@ -202,11 +216,27 @@ class Field {
       }
       e.isOpen = true;
       this.move.innerText = this.clickCount;
+      // IF LOOSE
+      if (e.isBomb) {
+        e.cellElement.innerText = '';
+        e.cellElement.style.backgroundImage = `url('./assets/mine.svg')`;
+        e.cellElement.style.backgroundColor = 'red';
+        this.remarkText.innerText = 'Game over. Try again';
+        this.remarkText.style.color = 'red';
+        this.remark.style.display = 'flex';
+        this.stopTimer();
+        console.log(this.gameTime);
+      }
+
+      //IF WIN
       if (this.cellMatrix.flat().filter((el) => !el.isBomb && el.isOpen).length === this.fieldSize ** 2 - this.minesCount) {
-        this.remarkText.innerText = 'Try again';
+        console.log(this.time, this.time.innerText)
+        this.remarkText.innerText = `Hooray! You found all mines in ${this.gameTime} seconds and ${this.clickCount} moves!`;
         this.remarkText.style.color = 'black';
         this.remark.style.display = 'flex';
         this.audioWin.play();
+        this.saveResult(this.gameTime, this.clickCount, this.fieldSize, this.minesCount);
+        this.stopTimer();
         this.game.removeEventListener('click', this.onFieldClick);
         this.game.removeEventListener('contextmenu', this.onFieldClick);
         this.size.removeEventListener('change', (e) => this.updateField(+this.size.value));
@@ -264,6 +294,48 @@ class Field {
     }
   }
 
+  startTimer() {
+    this.timer = setInterval(() => {
+      this.gameTime = 1 + +this.time.innerText;
+      this.time.innerText = `${this.gameTime}`;
+    }, 1000);
+  }
+
+  stopTimer() {
+    clearInterval(this.timer);
+  }
+
+  getDataFromLS() {
+    const dataFromLS = JSON.parse(localStorage.getItem('saves'));
+    return dataFromLS ?? [];
+  }
+
+  saveResult(t, c, s, m) {
+    let arr =[t,c,s,m];
+    const massiv = this.getDataFromLS();
+    massiv.push(arr);
+    if (massiv.length > 10) {
+      massiv.shift();
+    }
+    localStorage.setItem('saves', JSON.stringify(massiv));
+    this.fillTable(massiv);
+  }
+
+  fillTable(data) {
+    const lines = document.querySelectorAll('.table-line');
+    if(data && data[0] && data[0].length > 0) {
+      data.forEach((el,j)=> {
+        el.forEach((element, i)=>{
+          lines[j].children[i].innerText = element;
+        })
+      })
+  }
+  }
+}
+
+function reset() {
+  const time = document.querySelector('.time');
+  return time.innerText;
 }
 
 export default Field;
--- minesweeper/index.js
+++ minesweeper/index.js
@@ -6,63 +6,11 @@ const body = document.querySelector('body');
 const character = document.createElement('div');
 character.className = "character";
 
-
-const timeContainer = document.createElement('div');
-timeContainer.className = 'time-container';
-
-const timeText = document.createElement('p');
-timeText.className = "time-text";
-timeText.innerText = 'Game duration';
-timeContainer.append(timeText);
-
-const time = document.createElement('div');
-time.className = "time";
-timeContainer.append(time);
-
-character.append(timeContainer);
-
-
-const moveContainer = document.createElement('div');
-moveContainer.className = 'time-container';
-
-const moveText = document.createElement('p');
-moveText.className = "move-text";
-moveText.innerText = 'Number of clicks';
-moveContainer.append(moveText);
-
-const move = document.createElement('div');
-move.className = "move";
-moveContainer.append(move);
-
-character.append(moveContainer);
-
 const restart = document.createElement('button');
 restart.className = 'restart';
 restart.innerText = 'New game';
 character.append(restart);
 restart.addEventListener('click', () => { field.game.innerHtml = ""; field.updateField(); });
-
-body.append(character);
-
-const intervalID = setInterval(() => {
-  const currentTime = 1 + +time.innerText;
-  time.innerText = `${currentTime}`;
-}, 1000);
-// restart.addEventListener('click', ()=>clearInterval(intervalID));
-
-const remark = document.createElement('div');
-remark.className = 'remark';
-const remarkText = document.createElement('p');
-remark.append(remarkText);
-body.append(remark);
-
-const option = document.createElement('div');
-option.className = "character";
-
-const sound = document.createElement('button');
-sound.className = 'sound';
-sound.innerText = 'Sound';
-option.append(sound);
 
 const mineContainer = document.createElement('div');
 mineContainer.className = 'time-container';
@@ -76,7 +24,7 @@ const mine = document.createElement('div');
 mine.className = "mine";
 mineContainer.append(mine);
 
-option.append(mineContainer);
+character.append(mineContainer);
 
 
 const flagContainer = document.createElement('div');
@@ -91,13 +39,93 @@ const flag = document.createElement('div');
 flag.className = "flag-number";
 flagContainer.append(flag);
 
-option.append(flagContainer);
+character.append(flagContainer);
+
+
+
+const size = document.createElement('select');
+size.className = 'size';
+size.innerText = 'Size';
+
+const sSmall = document.createElement('option');
+sSmall.value = 10;
+sSmall.innerText = 'easy';
+const sMedium = document.createElement('option');
+sMedium.value = 15;
+sMedium.innerText = 'medium';
+const sBig = document.createElement('option');
+sBig.value = 25;
+sBig.innerText = 'hard';
+size.append(sSmall);
+size.append(sMedium);
+size.append(sBig);
+character.append(size);
+
+const mineVaryation = document.createElement('input');
+mineVaryation.className = 'mine-var';
+mineVaryation.type = 'number';
+mineVaryation.min = '10';
+mineVaryation.max = '99';
+mineVaryation.value = '10';
+character.append(mineVaryation);
+
+body.append(character);
+
+// const intervalID = setInterval(() => {
+//   const currentTime = 1 + +time.innerText;
+//   time.innerText = `${currentTime}`;
+// }, 1000);
+// restart.addEventListener('click', ()=>clearInterval(intervalID));
+
+const remark = document.createElement('div');
+remark.className = 'remark';
+const remarkText = document.createElement('p');
+remark.append(remarkText);
+body.append(remark);
+
+const option = document.createElement('div');
+option.className = "option";
+
+const sound = document.createElement('button');
+sound.className = 'sound';
+sound.innerText = 'Sound';
+option.append(sound);
+
+const timeContainer = document.createElement('div');
+timeContainer.className = 'time-container';
+
+const timeText = document.createElement('p');
+timeText.className = "time-text";
+timeText.innerText = 'Game duration';
+timeContainer.append(timeText);
+
+const time = document.createElement('div');
+time.className = "time";
+timeContainer.append(time);
+
+option.append(timeContainer);
+
+
+const moveContainer = document.createElement('div');
+moveContainer.className = 'time-container';
+
+const moveText = document.createElement('p');
+moveText.className = "move-text";
+moveText.innerText = 'Number of clicks';
+moveContainer.append(moveText);
+
+const move = document.createElement('div');
+move.className = "move";
+moveContainer.append(move);
+
+option.append(moveContainer);
+
+
 
 
 const theme = document.createElement('button');
 theme.className = 'theme';
 theme.innerText = 'Theme';
-// theme.style.dataTheme = "dark";
 const link = document.getElementById("theme-link");
 
 
@@ -122,51 +150,99 @@ function changeTheme() {
 
 option.append(theme);
 
-const size = document.createElement('select');
-size.className = 'size';
-size.innerText = 'Size';
-
-const sSmall = document.createElement('option');
-sSmall.value = 10;
-sSmall.innerText = '10';
-const sMedium = document.createElement('option');
-sMedium.value = 15;
-sMedium.innerText = '15';
-const sBig = document.createElement('option');
-sBig.value = 25;
-sBig.innerText = '25';
-size.append(sSmall);
-size.append(sMedium);
-size.append(sBig);
-
-option.append(size);
-
-const mineVaryation = document.createElement('input');
-mineVaryation.className = 'mine-var';
-mineVaryation.type = 'number';
-mineVaryation.min = '10';
-mineVaryation.max = '99';
-mineVaryation.value = '10';
-option.append(mineVaryation);
-
-
-
 body.append(option);
+
+const result = document.createElement('button');
+result.className = 'result';
+result.innerText = 'Result';
+body.append(result);
+result.addEventListener('click', viewResult);
+
+const table = document.createElement('div');
+table.className = 'table';
+const tableHeader = document.createElement('div');
+tableHeader.className = 'table-header';
+const headerTitles = ['duration', 'clicks', 'size', 'mines'];
+for (let i = 0; i < 4; i++) {
+  const headerCell = document.createElement('div');
+  headerCell.className = 'header-cell';
+  headerCell.innerText = headerTitles[i]
+  tableHeader.append(headerCell);
+}
+
+
+
+
+
+table.append(tableHeader)
+const tableBody = document.createElement('div');
+tableBody.className = 'table-body';
+for (let i = 0; i < 10; i++) {
+const tableLine = document.createElement('div');
+tableLine.className = 'table-line';
+tableBody.append(tableLine);
+for (let i = 0; i < 4; i++) {
+  const tableCell = document.createElement('div');
+  tableCell.className = 'table-cell';
+  tableLine.append(tableCell);
+}  
+}
+
+function viewResult() {
+  table.classList.toggle('flex');
+}
+
+
+table.append(tableBody);
+
+
+body.append(table);
 
 const field = new Field;
 field.createField();
+
+
+// const dataFromLS = JSON.parse(localStorage.getItem('saves'));
+// const lines = document.querySelectorAll('.table-line');
+
+// if(dataFromLS) {
+//   dataFromLS.forEach((el,j)=> {
+//     el.forEach((element, i)=>{
+//       lines[j].children[i].innerText = element;
+//     })
+//   })
+// }
+
+
 
 
 
 
 
 // console.log (`
-// -the game should end when the player reveals all cells that do not contain mines (win) or clicks on mine (lose) and related message is displayed at the end of the game
-// -sound accompaniment (on/off) 
-// -option to choose different themes for the game board (dark/light themes)
-// -implemented saving the latest 10 results using LocalStorage: +10
-// -implemented saving the state of the game: +10
-// -implement ability to change the size (easy - 10x10, medium - 15x15, hard - 25x25) and number of mines for each size of the field (from 10 to 99): 
+// - бомбы когда выигрываешь
+// -the (win)
+// -(dark/light themes)
+// -implemented saving the latest 10 results using LocalStorage
+// -implemented saving the state of the game
+// -(easy - 10x10, medium - 15x15, hard - 25x25) and number of mines for each size of the field (from 10 to 99): 
+// 
+//   Score: 160/180
+// + layout, design, responsive UI: +10
+// + at the beginning state of the game, the frame has size 10x10 and is filled with unopened cells. Should be 10 mines on field by default: +10
+// + when user click on cells - it should be opened and marked as one of the following state: empty cell, cell with number, or cell with mine: +10
+// + the game should end when the player reveals all cells that do not contain mines (win) or clicks on mine (lose) and related message is displayed at the end of the game: +10
+// + mines are placed after the first move, so that user cannot lose on the first move. +20
+// + user can mark “mined” cells using flags so as not to accidentally open them displaying the number of mines remaining and displaying number of used flags: +10
+// + the game should use color coding (using numbers and colors) to indicate the number of mines surrounding a revealed cell: +10
+// + the game can be restarted without reloading the page: +10
+// + game duration and number of clicks are displayed: +15
+// + when user opens a square that does not touch any mines, it will be empty and the adjacent squares will automatically open in all directions until reaching squares that contain numbers: +15
+// + sound accompaniment (on/off) when clicking on cell and at the end of the game: +10
+// + implement ability to change the size (easy - 10x10, medium - 15x15, hard - 25x25) and number of mines for each size of the field (from 10 to 99): +20
+// - implemented saving the latest 10 results using LocalStorage: 0
+// - implemented saving the state of the game: 0
+// + option to choose different themes for the game board (dark/light themes): +10
 // `)
 
 
